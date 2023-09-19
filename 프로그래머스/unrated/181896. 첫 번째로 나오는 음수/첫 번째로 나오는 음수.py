771ab37a77e8dc97952fbef14503ecd3dfb5c8ca
bvnohz <141910199+bvnohz@users.noreply.github.com>
2023-09-19 20:35:14 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -0,0 +1,11 @@
+
+def solution(num_list):
+    answer = -1
+    
+    for idx, val in enumerate(num_list):
+        if val < 0:
+            answer = idx
+            break
+    
+    return answer
+  
